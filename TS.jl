5f1b2ec6f068e3f6d309675dd66330f9e762a68e
LislPisl <eroesch@student.unimelb.edu.au>
2020-07-22 18:21:05 +1000

    tidy

--- TS.jl
+++ TS.jl
@@ -20,34 +20,37 @@ using Plots
 # ts = TS(simulate,1.,1.,1.,1.,1.,1.,1.)
 # ts.simulate(range(0,stop=10))
 
-function ff(u, ps, t)
-   #the five states
+# derivative for TS
+function dudt(u, ps, t)
+   # the five states
    sg4, sg1, sg4t4, N1, N2 = u
    # AHL Ara and params
    AHL, Ara, p1, p2, p3, p4, p5, p6, p7 = ps
+   # mass action kinetics
    dsg4 = AHL*p1
    dsg1 = Ara*p2 + N1*p3
-
-
    dsg4t4 = N2*p4
-
    dN1 = -sg4*p5 - sg4t4*p6 + sg1*p7
    dN2 = -dN1
-   print(N1+N2)
-   if N1+dN1 >= N1+N2 || N2+dN2 >= N1+N2
-      dN1=0
-      dn2=0
-   end
-
+   # control total nmbr of particles?
+   # if N1+dN1 >= N1+N2 || N2+dN2 >= N1+N2
+   #    dN1=0
+   #    dn2=0
+   # end
    du = [dsg4, dsg1, dsg4t4, dN1, dN2]
-   # println(du...)
    return du
 end
-u0=[2,2,3,1,10]
+
+# test setting
+u0=[2.,2,3,1,10]
 ps=[0.,2.,.1,.1,.1,.1,.1,.1,.1]
 tspan = (0.0,10.0)
-prob =ODEProblem(ff, u0, tspan, ps)
+prob =ODEProblem(dudt, u0, tspan, ps)
+
+# solve example
 sol = Array(solve(prob))
+
+# visualise sol
 plot(sol[1,:], label = "sg4")
 plot!(sol[2,:], label = "sg1")
 plot!(sol[3,:], label = "sg4t4")
